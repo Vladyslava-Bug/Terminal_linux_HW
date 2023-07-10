e71c51d69de8a09df58e5141cf5bf48d29a8dbb8
Vladyslava Zhukova <120176536+Vladyslava-Bug@users.noreply.github.com>
2023-07-10 19:33:27 +0100

    Update script.sh

--- HW1/script.sh
+++ HW1/script.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-cd dir_2 #1.Зайти в папку
-mkdir folder1 folder2 folder3 #2.Создать 3 папки 
-cd folder1 #3.Зайти в любую папку
-touch /d/dir_1/dir_2/folder1/{txt1.txt,txt2.txt,txt3.txt,json1.json,json2.json} #4.Создать 5 файлов (3 txt, 2 json)  
-mkdir A B C #5. Создать 3 папки  
-ls #6.Вывести список содержимого папки
-mv /d/dir_1/dir_2/folder1/{txt2.txt,json2.json} /d/dir_1/dir_2/folder1/B #7.Переместить любые 2 файла в любую другую папку
+#1.Go to the folder
+cd dir_2 
+#2.Create 3 folders
+mkdir folder1 folder2 folder3 
+#3.Go to any folder
+cd folder1 
+#4.Create 5 files (3 txt, 2 json)
+touch /d/dir_1/dir_2/folder1/{txt1.txt,txt2.txt,txt3.txt,json1.json,json2.json} 
+#5. Create 3 folders
+mkdir A B C 
+#6.Show the list folder contents
+ls 
+#7.Move any 2 files you created to any other folder
+mv /d/dir_1/dir_2/folder1/{txt2.txt,json2.json} /d/dir_1/dir_2/folder1/B 
